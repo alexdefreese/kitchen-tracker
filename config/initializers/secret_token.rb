5fd9c1b13a0ea1d11521a18961db6ae5f0f50f59
Alex DeFreese <alexdefreese@gmail.com>
2013-04-06 18:01:33 -0400

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+KitchenTracker::Application.config.secret_token = '75dc29c0dd827916f18de02649c2e06c0bd140cd0c7b7608bd90df67c21090a63dbcd96bf6c312b7a7ae3f7ba2b9d02ff96acc589527dbd1d7d5227cd8bf3ee8'
