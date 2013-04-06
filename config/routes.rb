8f08475d37816a847263761bdd83a525484bcb97
Alex DeFreese <alexdefreese@gmail.com>
2013-04-06 18:20:24 -0400

    Added Foods controller.

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,18 @@
 KitchenTracker::Application.routes.draw do
+  get "foods/index"
+
+  get "foods/show"
+
+  get "foods/new"
+
+  get "foods/create"
+
+  get "foods/edit"
+
+  get "foods/update"
+
+  get "foods/destroy"
+
   get "kitchens/new"
 
   get "kitchens/create"
