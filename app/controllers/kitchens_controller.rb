9f20fb864634fd8ac2ca4c7ae8a6ba5af527a2ce
Alex DeFreese <alexdefreese@gmail.com>
2013-04-07 11:13:19 -0400

    Kitchen Controller updated to populate class variables.

--- app/controllers/kitchens_controller.rb
+++ app/controllers/kitchens_controller.rb
@@ -1,16 +1,30 @@
 class KitchensController < ApplicationController
+  
   def new
+    @kitchen = Kitchen.new()
   end
 
   def create
+    @kitchen = Kitchen.new(params[:kitchen])
+    if @kitchen.save
+      flash[:success] = "Kitchen Created."
+      redirect_to root_path
+    else
+      render :new
+    end
   end
 
   def edit
+    @kitchen = Kitchen.find(params[:id])
   end
 
   def update
+    @kitchen = Kitchen.find(params[:id])
+    @kitchen.update_attributes(params[:event])
   end
 
   def destroy
+    @kitchen = Kitchen.find(params[:id])
+    @kitchen.destroy
   end
 end
